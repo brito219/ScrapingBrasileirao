23bb9f9e583741265b3b52aae264eb159ed93554
João Pedro S. de brito <jpxzd1@gmail.com>
2025-08-18 23:12:31 -0400

    cleanup+test: exclusão de arquivos e aumento no rigor da depuração

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from scraping.classificacoes.geral import Geral
-from scraping.classificacoes.ultimasDezRodadas import UltimasDezRodadas
-from scraping.classificacoes.mandante import Mandante   
-from scraping.classificacoes.returno import Returno 
-from scraping.classificacoes.turno import Turno
-from scraping.classificacoes.visitante import Visitante
-from db.conexao import Conexao
+from etl.extracao.scraping.classificacoes.geral import Geral
+from etl.extracao.scraping.classificacoes.ultimasDezRodadas import UltimasDezRodadas
+from etl.extracao.scraping.classificacoes.mandante import Mandante   
+from etl.extracao.scraping.classificacoes.returno import Returno 
+from etl.extracao.scraping.classificacoes.turno import Turno
+from etl.extracao.scraping.classificacoes.visitante import Visitante
+from data.conexao import Conexao
 
 if __name__ == "__main__":
     print("Testando conexão com o SQL Server...")
